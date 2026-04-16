05c25ec77c868b999ceb1d25a1b8b2b4f0121b2c
harihifihost <hariprasaths@hifihost.com>
2026-04-16 22:47:40 +0530

    fix utils case issue

--- src/Pages/Myorders.jsx
+++ src/Pages/Myorders.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../api/api";
 import { useAuth } from "../Context/AuthContext";
-import { formatDate } from "../utils/helpers";
+import { formatDate } from "../utils/helpers"
 
 function MyOrders() {
   const [orders, setOrders] = useState([]);
